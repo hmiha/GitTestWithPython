ed5b264b54dab42f5701872c389818c5e23c621b
hmiha <hajimemihara1108@gmail.com>
2014-05-23 17:17:52 +0900

    fibonacci number

---
+++ fib.py
@@ -0,0 +1,13 @@
+import time
+
+def fib_tail(n, a = 1, b = 0):
+    if n == 0:
+        return a 
+    else:
+      return fib_tail(n-1, a+b, a)
+
+n = input("n = ")
+t = time.clock()
+
+print fib_tail(n) 
+print time.clock() - t
